32f0483b404764e7e3fbd4ea366159a62f741338
Dave030829 <gyorkyd030829@gmail.com>
2024-05-31 08:50:57 +0200

    Final version

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,72 +1,53 @@
 <!DOCTYPE html>
-<html lang="hu">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Weboldal</title>
+    <title>Home Page</title>
     <style>
         body {
             font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
             margin: 0;
             padding: 0;
-        }
-
-        header {
-            background-color: purple;
-            color: #fff;
-            padding: 10px 20px;
             display: flex;
-            justify-content: space-between;
+            justify-content: center;
             align-items: center;
+            height: 100vh;
         }
-
-        nav ul {
-            list-style-type: none;
-            margin: 0;
-            padding: 0;
-            display: flex;
+        .container {
+            background: white;
+            padding: 20px 30px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            text-align: center;
+            border-radius: 8px;
         }
-
-        nav ul li {
-            margin-right: 20px;
+        h1 {
+            color: #333;
         }
-
-        nav ul li:last-child {
-            margin-right: 0;
+        p {
+            color: #666;
+            font-size: 1.1em;
         }
-
-        nav ul li a {
-            color: #fff;
+        a {
+            display: inline-block;
+            margin-top: 20px;
+            padding: 10px 20px;
+            font-size: 1em;
+            color: white;
+            background-color: #007BFF;
             text-decoration: none;
-            font-size: 18px;
+            border-radius: 5px;
+            transition: background-color 0.3s ease;
         }
-
-        .icons {
-            width: 30px;
-            height: 30px;
-            fill: #fff;
-            margin-left: 10px;
-            cursor: pointer;
+        a:hover {
+            background-color: #0056b3;
         }
-
-        
     </style>
 </head>
 <body>
-    <header>
-        <nav>
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Computers</a></li>
-                <li><a href="#">Account</a></li>
-            </ul>
-        </nav>
-        <div class="icons">
-        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M16 7C16 9.20914 14.2091 11 12 11C9.79086 11 8 9.20914 8 7C8 4.79086 9.79086 3 12 3C14.2091 3 16 4.79086 16 7Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M12 14C8.13401 14 5 17.134 5 21H19C19 17.134 15.866 14 12 14Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
-
-        <svg version="1.1" id="_x32_" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <style type="text/css"> .st0{fill:#000000;} </style> <g> <path class="st0" d="M483.157,164.432H350.464l-60.034,58.983c-8.131,7.986-18.893,12.385-30.292,12.385 c-11.69,0-22.64-4.594-30.836-12.935c-15.835-16.132-16.414-41.598-1.942-58.432H28.843C12.914,164.432,0,177.346,0,193.282v9.617 c0,15.697,12.552,28.451,28.176,28.806l31.097,190.436c3.761,23.06,23.697,39.996,47.062,39.996h299.279 c23.372,0,43.294-16.936,47.062-39.996l31.104-190.436c15.632-0.333,28.22-13.088,28.22-28.806v-9.617 C512,177.346,499.086,164.432,483.157,164.432z M153.571,410.141c-2.558,2.833-6.203,4.457-10.015,4.457h-7.529 c-6.718,0-12.407-4.942-13.35-11.588l-16.559-117.083c-0.544-3.87,0.608-7.783,3.166-10.733c2.559-2.942,6.277-4.638,10.182-4.638 h11.828c6.906,0,12.689,5.211,13.407,12.074l12.262,117.09C157.354,403.517,156.122,407.3,153.571,410.141z M230.737,410.532 c-2.53,2.595-6.008,4.066-9.639,4.066h-7.71c-7.132,0-13.03-5.558-13.45-12.675l-6.957-117.09 c-0.218-3.704,1.101-7.342,3.645-10.038c2.55-2.703,6.095-4.24,9.812-4.24h12.016c7.326,0,13.306,5.856,13.472,13.168l2.652,117.09 C234.658,404.438,233.274,407.945,230.737,410.532z M312.02,401.923c-0.428,7.117-6.327,12.675-13.458,12.675h-7.711 c-3.63,0-7.102-1.471-9.631-4.066c-2.544-2.587-3.928-6.094-3.848-9.718l2.66-117.09c0.16-7.312,6.146-13.168,13.473-13.168h12.015 c3.703,0,7.261,1.537,9.805,4.24c2.544,2.696,3.862,6.334,3.645,10.038L312.02,401.923z M405.832,285.927l-16.56,117.083 c-0.934,6.645-6.631,11.588-13.341,11.588h-7.53c-3.819,0-7.464-1.624-10.016-4.457c-2.558-2.841-3.79-6.624-3.391-10.421 l12.255-117.09c0.717-6.863,6.508-12.074,13.407-12.074h11.828c3.906,0,7.616,1.696,10.175,4.638 C405.223,278.144,406.382,282.057,405.832,285.927z"></path> <path class="st0" d="M277.618,210.363l119.88-117.756c9.828-9.661,9.965-25.452,0.312-35.279 c-9.653-9.827-25.452-9.972-35.278-0.318L242.658,174.78c-9.827,9.653-9.965,25.444-0.319,35.271 C252,219.878,267.783,220.023,277.618,210.363z"></path> </g> </g></svg>
-        </div>
-    </header>
-    <!-- Ide jöhetnek további tartalmak -->
+    <div class="container">
+        <h1>Welcome to the IoT Project</h1>
+        <p>This is the home page.</p>
+        <a href="/admin">Admin Panel</a>
+    </div>
 </body>
 </html>
